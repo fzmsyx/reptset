a4d620e9eceb633aca4fa6d680bec6a05162cbbc
huazaiass <62973675+huazaiass@users.noreply.github.com>
2020-04-02 09:57:23 +0900

    Update HelloWorld.java

--- reptest/src/com/mmmp/lesson/HelloWorld.java
+++ reptest/src/com/mmmp/lesson/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("又是充满痛苦的一天啊!");
          System.out.println("又是充满痛苦的一天啊aaaaaaa!");
+        int i = 12312412 ;
     }
 }
