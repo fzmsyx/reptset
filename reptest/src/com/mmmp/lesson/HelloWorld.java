846048d702ecb7323e63096a48de0db643f6e4a2
sophia <sophiasaww@gmail.com>
2020-04-02 10:08:48 +0900

    苏第二次修改

--- reptest/src/com/mmmp/lesson/HelloWorld.java
+++ reptest/src/com/mmmp/lesson/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("又是充满痛苦的一天啊!");
          System.out.println("又是充满痛苦的一天啊aaaaaaa!");
         int i = 12312412 ;
+        String str="乌拉拉黑暗魔法"
     }
 }
