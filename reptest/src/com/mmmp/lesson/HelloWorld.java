7159f652da8d9075b8883c24e5417068f3eb7ad3
fzmsyx <62974162+fzmsyx@users.noreply.github.com>
2020-04-02 09:36:43 +0900

    Update HelloWorld.java
    新增了输出语句

--- reptest/src/com/mmmp/lesson/HelloWorld.java
+++ reptest/src/com/mmmp/lesson/HelloWorld.java
@@ -3,5 +3,6 @@ package com.mmmp.lesson;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("又是充满痛苦的一天啊!");
+         System.out.println("又是充满痛苦的一天啊aaaaaaa!");
     }
 }
